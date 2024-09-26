4ac19d533ae4d4e32b3ca3d293ad6c08ef72fb66
Shubham Gorai <cptnshubham39@gmail.com>
2024-09-26 14:13:49 +0530

    Update display.py

--- display.py
+++ display.py
@@ -1,18 +1,23 @@
-# File for the oled display
+import board
+import busio
+import adafruit_ssd1305
+from PIL import Image, ImageDraw, ImageFont
+import time
 
-from luma.core.interface.serial import i2c
-from luma.core.render import canvas
-from time import sleep
-from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
+i2c = busio.I2C(board.SCL, board.SDA)
+
+display = adafruit_ssd1305.SSD1305_I2C(128, 32, i2c)
 
 # OLED display stuff
 
-serial = i2c(port=1, address=0x3C)
-device = ssd1306(serial, rotate=0)
 
 def draw(mylist):
 	index = 0
-	with canvas(device) as draw:
-		for item in mylist:
-			draw.text((0, index), item, fill="white")
-			index += 10
+	image = Image.new('1', (128, 32)) 
+	draw = ImageDraw.Draw(image)
+	for item in mylist:
+		draw.text((0, index), item, fill="white")
+		index += 10
+	
+	display.image(image)
+	display.show()
