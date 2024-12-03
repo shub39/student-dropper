26236a8e07c4c9d9479e7504f78c334077ddc296
shub39 <cptnshubham39@gmail.com>
2024-12-03 18:57:29 +0530

    :wrench: finished venv script
    * Hopefully

--- setup_venv.sh
+++ setup_venv.sh
@@ -9,7 +9,11 @@ source .venv/bin/activate
 
 # install apt dependencies
 sudo apt update
+sudo apt upgrade
 sudo apt install libkms++-dev libfmt-dev libdrm-dev # might require more...
 
 # install pip dependencies
 uv pip install -r requirements.txt
+
+# need extra args for this one becuz they haven't updated yet :(
+uv pip install rpi-libcamera -C setup-args="-Dversion=unknown"
